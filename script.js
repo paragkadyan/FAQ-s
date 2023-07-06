bcd3aa6bc51b75fe5cea091fbdc8152d09525214
Parag <kadyanparag@gmail.com>
2023-07-06 15:28:13 +0530

    used IF/Else

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ accordians.forEach(accordian => {
     const answer = accordian.querySelector(".answer");
 
     accordian.addEventListener("click", () => {
-        icon.classList.toggle("active");
-        answer.classList.toggle("active");
+        if(icon.classList.contains("active")){
+            icon.classList.remove("active");
+            answer.style.maxHeight = null;
+        }
+        else {
+            icon.classList.add("active");
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        }
     })
 })
